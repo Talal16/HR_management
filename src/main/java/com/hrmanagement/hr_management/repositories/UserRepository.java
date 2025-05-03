1fb26ac98cc6da5e2723e1b6e4027c426dca1163
talal16 <talal16046@stemegypt.edu.eg>
2025-05-03 14:54:48 +0300

    update the userRepository.java
    update with JPQL

--- src/main/java/com/hrmanagement/hr_management/repositories/UserRepository.java
+++ src/main/java/com/hrmanagement/hr_management/repositories/UserRepository.java
@@ -1,19 +1,33 @@
 package com.hrmanagement.hr_management.repositories;
 
+ import com.hrmanagement.hr_management.models.User;
+ import org.springframework.data.jpa.repository.JpaRepository;
+ import org.springframework.data.jpa.repository.Query;
+ import org.springframework.data.repository.query.Param;
+ import org.springframework.stereotype.Repository;
 
-import com.hrmanagement.hr_management.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+ import java.time.LocalDateTime;
+ import java.util.List;
 
-import java.time.LocalDateTime;
-import java.util.List;
+ @Repository
+ public interface UserRepository extends JpaRepository<User, Long> {
 
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    User getUserById (Long userId);
-    List<User> findUsersByDepartmentId(Long departmentId);
-    User findUserByEmail (String email);
-    List<User> findUsersByManagerId(Long managerId);
-    List<User> findUsersByRoleId(Long RoleId);
-    List<User> findUsersByHireDateBetween(LocalDateTime startDate, LocalDateTime endDate);
+
+     @Query("SELECT i FROM User u WHERE u.id = :userId")
+     User getUserById(@Param("userid") Long userId);
+ 
+     @Query("SELECT u FROM User u WHERE u.department.id = :departmentId")
+     List<User> findUsersByDepartmentId(@Param("departmentId") Long departmentId);
+
+     @Query("SELECT u FROM User u WHERE u.email = :email")
+     User findUserByEmail(@Param("email") String email);
+
+     @Query("SELECT u FROM User u WHERE u.manager.id = :managerId")
+     List<User> findUsersByManagerId(@Param("managerId") Long managerId);
+
+     @Query("SELECT u FROM User u WHERE u.role.id = :roleId")
+     List<User> findUsersByRoleId(@Param("roleId") Long roleId);
+
+     @Query("SELECT u FROM User u WHERE u.hireDate BETWEEN :startDate AND :endDate")
+     List<User> findUsersByHireDateBetween(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
  }
