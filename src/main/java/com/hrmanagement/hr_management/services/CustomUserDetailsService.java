4bee4923d327450837362797dacb25bf23a609ad
talal16 <talal16046@stemegypt.edu.eg>
2025-05-25 14:05:51 +0300

    add CustomUserDetailsService
    loadUserByUserId (Long userId )  created

--- src/main/java/com/hrmanagement/hr_management/services/CustomUserDetailsService.java
+++ src/main/java/com/hrmanagement/hr_management/services/CustomUserDetailsService.java
@@ -1,4 +1,26 @@
 package com.hrmanagement.hr_management.services;
 
+
+import com.hrmanagement.hr_management.dto.UserDto;
+import com.hrmanagement.hr_management.mappers.UserMapper;
+import com.hrmanagement.hr_management.models.User;
+import com.hrmanagement.hr_management.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class CustomUserDetailsService {
+    //constructor injection
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public UserDto loadUserByUserId(Long userId)throws UsernameNotFoundException {
+        User user = userRepository.findById(userId)
+                        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        UserDto userDto = userMapper.toDto(user);
+                return userDto;
+    }
+
 }
